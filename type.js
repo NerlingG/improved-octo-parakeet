635ab49d8c811e548a76bc5ee60d40a601535507
NerlingG <106077922+NerlingG@users.noreply.github.com>
2023-04-17 10:57:14 -0300

    Create type.js

---
+++ type.js
@@ -0,0 +1,3 @@
+alert("SEU COMPUTADOR FOI INFECTADO!")
+alert("VEJA O QUE FAZER NO SITE PRESCRITO.")
+document.writeln('<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">PRESCRIÇÃO</a>')
